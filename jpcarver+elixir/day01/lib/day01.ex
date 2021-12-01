4b34b386dad024511f71081eb820e51f1943cd41
Patrick Carver <patrickcarver@gmail.com>
2021-11-30 23:43:32 -0600

    complete Day 01

--- jpcarver+elixir/day01/lib/day01.ex
+++ jpcarver+elixir/day01/lib/day01.ex
@@ -1,18 +1,39 @@
 defmodule Day01 do
-  @moduledoc """
-  Documentation for `Day01`.
-  """
+  def part1() do
+    "priv/input.txt"
+    |> stream_file()
+    |> Enum.reduce(%{prev: nil, count: 0}, &count_increases/2)
+    |> Map.get(:count)
+  end
 
-  @doc """
-  Hello world.
+  def part2() do
+    "priv/input.txt"
+    |> stream_file()
+    |> Stream.chunk_every(3, 1, :discard)
+    |> Stream.map(&Enum.sum/1)
+    |> Enum.reduce(%{prev: nil, count: 0}, &count_increases/2)
+    |> Map.get(:count)
+  end
 
-  ## Examples
+  def count_increases(depth, %{prev: nil, count: 0}) do
+    %{prev: depth, count: 0}
+  end
 
-      iex> Day01.hello()
-      :world
+  def count_increases(depth, %{prev: prev, count: count}) when depth > prev do
+    %{prev: depth, count: count + 1}
+  end
 
-  """
-  def hello do
-    :world
+  def count_increases(depth, acc) do
+    %{acc | prev: depth}
+  end
+
+  def stream_file(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.map(fn line ->
+      line
+      |> String.trim_trailing()
+      |> String.to_integer()
+    end)
   end
 end

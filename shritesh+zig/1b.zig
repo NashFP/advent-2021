59cce797fd5e3174793126b192d6230e07096357
Shritesh Bhattarai <shr@ite.sh>
2021-12-01 05:38:54 -0500

    shritesh/1b streaming

--- shritesh+zig/1b.zig
+++ shritesh+zig/1b.zig
@@ -6,32 +6,34 @@ const stdin = std.io.getStdIn().reader();
 
 pub fn main() !void {
     var buffer: [4]u8 = undefined;
-    var depths: [2000]u16 = undefined;
-    var idx: usize = 0;
 
-    while (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        depths[idx] = try fmt.parseInt(u16, line, 10);
-        idx += 1;
-    }
+    var first: ?u16 = null;
+    var second: ?u16 = null;
 
-    print("{}\n", .{countIncrements(depths[0..idx])});
-}
-
-fn countIncrements(depths: []const u16) usize {
-    var i: usize = 0;
-    var increments: usize = 0;
     var last: ?u16 = null;
 
-    while (i + 2 < depths.len) : (i += 1) {
-        const sum = depths[i] + depths[i + 1] + depths[i + 2];
+    var increments: usize = 0;
 
-        if (last) |l| {
-            if (sum > l)
-                increments += 1;
+    while (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        const third = try fmt.parseInt(u16, line, 10);
+
+        if (first) |f| {
+            if (second) |s| {
+                const sum = f + s + third;
+
+                if (last) |l| {
+                    if (sum > l) {
+                        increments += 1;
+                    }
+                }
+
+                last = sum;
+            }
         }
 
-        last = sum;
+        first = second;
+        second = third;
     }
 
-    return increments;
+    print("{}\n", .{increments});
 }

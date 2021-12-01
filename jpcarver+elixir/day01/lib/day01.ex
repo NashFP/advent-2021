983385153239f2c75e4991b4d07624c0eb0c4b97
Patrick Carver <patrickcarver@gmail.com>
2021-11-30 18:01:30 -0600

    create Day 01

---
+++ jpcarver+elixir/day01/lib/day01.ex
@@ -0,0 +1,18 @@
+defmodule Day01 do
+  @moduledoc """
+  Documentation for `Day01`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day01.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

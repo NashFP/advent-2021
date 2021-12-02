e165c92918790004f3accdd786a855bc919b688c
Patrick Carver <patrickcarver@gmail.com>
2021-12-02 04:16:41 -0600

    start Day 02

---
+++ jpcarver+elixir/day02/lib/day02.ex
@@ -0,0 +1,18 @@
+defmodule Day02 do
+  @moduledoc """
+  Documentation for `Day02`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day02.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

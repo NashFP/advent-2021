18adddd0511752b8a9e840914def89e7a784f2a0
Patrick Carver <patrickcarver@gmail.com>
2021-12-04 18:30:51 -0600

    Day04 refactor BingoChecker

--- jpcarver+elixir/day04/lib/Day04/bingo_checker.ex
+++ jpcarver+elixir/day04/lib/Day04/bingo_checker.ex
@@ -1,22 +1,15 @@
 defmodule Day04.BingoChecker do
-  def is_bingo(board, %{x: x, y: y}) do
-    has_vertical = check_bingo_vertical(board, x)
-    has_horizontal = check_bingo_horizontal(board, y)
+  def is_bingo(board, %{x: column, y: row}) do
+    has_vertical = check_bingo(board, fn %{x: x} -> x == column end)
+    has_horizontal = check_bingo(board, fn %{y: y} -> y == row end)
 
     Enum.any?([has_vertical, has_horizontal])
   end
 
-  def check_bingo_vertical(board, column) do
+  def check_bingo(board, filter_func) do
     board
     |> Map.values()
-    |> Enum.filter(fn %{x: x} -> x == column end)
-    |> Enum.all?(fn %{is_marked: is_marked} -> is_marked end)
-  end
-
-  def check_bingo_horizontal(board, row) do
-    board
-    |> Map.values()
-    |> Enum.filter(fn %{y: y} -> y == row end)
+    |> Enum.filter(filter_func)
     |> Enum.all?(fn %{is_marked: is_marked} -> is_marked end)
   end
 end
